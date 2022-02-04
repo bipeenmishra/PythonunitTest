eea4b06e4177ca4f42d581fddbfbb58d7f50ac86
Bipeen Mishra <bipeen.mishra@marketredesign.com>
2022-02-04 11:27:58 +0530

    Initial Commit

--- data.py
+++ data.py
@@ -6,4 +6,17 @@ csv = 'demo_data_churn.csv'
 
 df2 = pd.read_csv(csv)
 su = df2['gross_sales'].sum()
-print("Total Sales:", round(su))
+x = 2719153786.144917
+
+
+class TestingSum(unittest.TestCase):
+
+    def test_negative(self):
+        firstValue = su
+        secondValue = 0
+        message ="Total sales:",su
+
+        self.assertEqual(firstValue, secondValue, message)
+
+if __name__ == '__main__':
+    unittest.main()
